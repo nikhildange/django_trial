c93644a03fe4e9850f7a14d7eb0f5960100133fe
nikhildange <nikdange.me@gmail.com>
2017-08-30 01:37:14 +0530

    basic token authentication n permission

--- src/wj_server/user_app/views.py
+++ src/wj_server/user_app/views.py
@@ -1,20 +1,25 @@
 from rest_framework import generics
 from .models import UserProfile, JobApplied
+from .permissions import IsAdminOrReadOnly
 from .serializers import UserProfileSerializer, UserJobSerializer
 
 class UserProfileList(generics.ListCreateAPIView):
 	queryset = UserProfile.objects.all()
 	serializer_class = UserProfileSerializer
+	permission_classes = (IsAdminOrReadOnly,)
 
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = UserProfile.objects.all()
 	serializer_class = UserProfileSerializer
+	permission_classes = (IsAdminOrReadOnly,)
 
 class UserJobList(generics.ListCreateAPIView):
 	queryset = JobApplied.objects.all()
 	serializer_class = UserJobSerializer
+	permission_classes = (IsAdminOrReadOnly,)
 
 class UserJobDestroy(generics.DestroyAPIView):
 	queryset = JobApplied.objects.all()
 	serializer_class = UserJobSerializer
+	permission_classes = (IsAdminOrReadOnly,)
 		
--- src/wj_server/wj_server/settings.py
+++ src/wj_server/wj_server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_mysql',
     'job_app',
     'user_app',
@@ -135,3 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+            'rest_framework.authentication.TokenAuthentication',
+        )
+}
